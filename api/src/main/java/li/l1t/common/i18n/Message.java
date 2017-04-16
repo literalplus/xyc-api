5d307e3e26d6f6f8ff0940df13d82ef58d5bde7e
Literallie <git@l1t.li>
2017-04-17 01:35:36 +0200

    Fix unable to extend Message due to private constructor
    This should not normally be done, but only where legacy compatibility
     requires it

--- api/src/main/java/li/l1t/common/i18n/Message.java
+++ api/src/main/java/li/l1t/common/i18n/Message.java
@@ -46,6 +46,30 @@
  * SOFTWARE.
  */
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016-2017 Philipp Nowak (Literallie)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package li.l1t.common.i18n;
 
 import java.util.Arrays;
@@ -64,7 +88,7 @@ public class Message {
     private final Object[] arguments;
     private Message fallback;
 
-    private Message(String staticText, String key, Object... arguments) {
+    protected Message(String staticText, String key, Object... arguments) {
         this.key = key;
         this.arguments = arguments;
         this.staticText = staticText;
